d3c8ee706f3137f996c68d26711aae39f3d13b0a
BadAllOff <onlyforsupport@mail.ru>
2015-09-05 00:15:08 +0500

    Delete: Meeting and stylesheets

--- app/models/meeting.rb
+++
@@ -1,9 +0,0 @@
-class Meeting < ActiveRecord::Base
-  belongs_to :user, dependent: :destroy
-  validates :title, :topics, :starts_at, :ends_at, :venue, :max_members, presence: true
-  validates :title, :topics, length: {minimum: 10}
-  validates_datetime :ends_at, after: :starts_at
-  validates :max_members, numericality: { only_integer: true,
-                                          greater_than_or_equal_to: 2,
-                                          less_than: 1000  }
-end
