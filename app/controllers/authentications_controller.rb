8b78b4eb618aab1ccc542ffe4882233481935ff2
BadAllOff <onlyforsupport@mail.ru>
2015-09-15 04:33:00 +0500

    ADDED: Facebook omniatuh login feature

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -12,23 +12,60 @@ class AuthenticationsController < ApplicationController
 
   end
 
+  def failure
+    redirect_to new_user_registration_path
+  end
+
   def twitter
     omni = request.env["omniauth.auth"]
     # raise omni.to_yaml
     authentication = Authentication.find_by_provider_and_uid(omni['provider'], omni['uid'])
 
     if authentication
-      flash[:notice] = "Logged in Successfully"
+      flash[:notice] = "Logged in Successfully with Twitter"
       sign_in_and_redirect User.find(authentication.user_id)
     elsif current_user
       token = omni['credentials'].token
       token_secret = omni['credentials'].secret
 
       current_user.authentications.create!(:provider => omni['provider'], :uid => omni['uid'], :token => token, :token_secret => token_secret)
-      flash[:notice] = "Authentication successful."
+      flash[:notice] = "Authentication successful. With Twitter"
       sign_in_and_redirect current_user
     else
       user = User.new
+      user.apply_omniauth(omni)
+
+      if user.save
+        flash[:notice] = "Logged in. With Twitter"
+        sign_in_and_redirect User.find(user.id)
+      else
+        session[:omniauth] = omni.except('extra')
+        redirect_to new_user_registration_path
+      end
+    end
+  end
+
+  def facebook
+    omni = request.env["omniauth.auth"]
+    # raise omni.to_yaml
+    authentication = Authentication.find_by_provider_and_uid(omni['provider'], omni['uid'])
+
+    if authentication
+      flash[:notice] = "Logged in Successfully with Facebook"
+      sign_in_and_redirect User.find(authentication.user_id)
+    elsif current_user
+      token = omni['credentials'].token
+      token_secret = ''
+
+      current_user.authentications.create!(:provider => omni['provider'], :uid => omni['uid'], :token => token, :token_secret => token_secret)
+      flash[:notice] = "Authentication successful. With Facebook"
+      sign_in_and_redirect current_user
+    else
+      user = User.new
+      user.email      = omni['extra']['raw_info'].email
+      user.first_name = omni['extra']['raw_info'].first_name
+      user.last_name  = omni['extra']['raw_info'].last_name
+
       user.apply_omniauth(omni)
 
       if user.save
