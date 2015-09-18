8f9734f733fec5fb5048505571a198f60855fa85
BadAllOff <onlyforsupport@mail.ru>
2015-09-18 15:32:53 +0500

    FIX: Added meta tag to layout and smtp to mailer in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
+  config.action_mailer.default_url_options = {:host => "conversation-app.herokuapp.com" } #todo - CHANGE IT!
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'mail.google.com',
+      user_name:            ENV['SMTP_PASSWORD'],
+      password:             ENV['SMTP_EMAIL'],
+      authentication:       'login',
+      enable_starttls_auto: true
+  }
 end
